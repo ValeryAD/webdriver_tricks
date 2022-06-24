9ebca592f26d2845259a0425c8f220358d933360
Valery Doroshko <DoroshkoValery@gmail.com>
2022-06-24 09:52:19 +0300

    add waits

--- src/main/java/com/github/valeryAD/training/waits/ExplicitWaitExamine.java
+++ src/main/java/com/github/valeryAD/training/waits/ExplicitWaitExamine.java
@@ -17,15 +17,28 @@ public class ExplicitWaitExamine {
     driver = DriverSingletone.getInstance();
     driver.get("https://www.google.com/");
     WebElement searchField = getElement(By.name("q"));
+    System.err.println("input color:" + searchField.getCssValue("color"));
     searchField.sendKeys(searchString);
+    //There's no need in this wait just for demonstration
+    waitForCssProperty(searchField);
+
     //searchField.sendKeys(Keys.ENTER);
     WebElement firstSearchResult = new WebDriverWait(driver, Duration.ofSeconds(7)).until(
         ExpectedConditions.elementToBeClickable(By.xpath("//ul/li[@data-view-type and @role='presentation'][1]")));
-        firstSearchResult.click();
+    firstSearchResult.click();
+
+    driver.close();
   }
 
+  //explicit wait
   public static WebElement getElement(By by){
     return new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(by));
   }
 
+  //explicit wait with custom condition
+  public static void waitForCssProperty(WebElement element){
+    new WebDriverWait(driver, Duration.ofSeconds(10)).until((driver) ->  element.getCssValue("color").equals("rgba(0, 0, 0, 0.87)"));
+  }
+
+  //fluent wait
 }
